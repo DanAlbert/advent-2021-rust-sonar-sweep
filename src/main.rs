ba72c1cf763973ffc35f262c9e60129f1908c124
Dan Albert <danalbert@google.com>
2022-10-21 14:14:44 -0700

    Basic implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+const SEA_FLOOR: &[i32] = &[199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
 fn main() {
-    println!("Hello, world!");
+    let mut last_depth: Option<i32> = None;
+    let mut increases = 0;
+    for depth in SEA_FLOOR {
+        if let Some(n) = last_depth {
+            if n < *depth {
+                increases += 1
+            }
+        }
+        last_depth = Some(*depth);
+    }
+    println!("Increases {}", increases);
 }
