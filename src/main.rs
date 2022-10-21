a0d9e55bcf0592922297ea0496e5b9e9fe54ecda
Dan Albert <danalbert@google.com>
2022-10-21 14:33:16 -0700

    Use the problem input file. Complete.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-const SEA_FLOOR: &[i32] = &[199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+const MY_INPUT: &str = include_str!("input");
 
-fn main() {
+fn count_increases(depths: &[i32]) -> i32 {
     let mut last_depth: Option<i32> = None;
     let mut increases = 0;
-    for depth in SEA_FLOOR {
+    for depth in depths {
         if let Some(n) = last_depth {
             if n < *depth {
                 increases += 1
@@ -11,5 +11,21 @@ fn main() {
         }
         last_depth = Some(*depth);
     }
-    println!("Increases {}", increases);
+    increases
+}
+
+#[test]
+fn test_count_increases() {
+    assert_eq!(
+        7,
+        count_increases(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    )
+}
+
+fn main() {
+    let floor: Vec<i32> = MY_INPUT
+        .split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    println!("Increases {}", count_increases(&floor));
 }
